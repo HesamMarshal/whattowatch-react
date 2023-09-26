d9f7e3881e70a7df3ccc3c7e9105c20b427fb92a
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-26 16:29:46 +0330

    remove unused state of count

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,6 @@ import NewSeriesList from "./components/NewSeriesList";
 import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
