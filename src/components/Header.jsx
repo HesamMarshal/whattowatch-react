226f6d9dedfd4fd6e5a0fad68351c8a89e71198d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-26 10:56:01 +0330

    add name and search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          What To Watch
+          <span className="separator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <input
+            type="text"
+            id="searchItem"
+            name="searchItem"
+            placeholder="Search ..."
+            className="headerSearchInput"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
