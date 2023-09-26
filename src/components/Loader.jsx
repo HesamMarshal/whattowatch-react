27fb9edaec721f862fe0e032d25cc88e7f9e6e16
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-26 12:51:55 +0330

    rename Loader to Loading

--- src/components/Loader.jsx
+++
@@ -1,5 +0,0 @@
-function Loader() {
-  return <div>Loading Data ...</div>;
-}
-
-export default Loader;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,5 @@
+function Loading() {
+  return <div>Loading Data ...</div>;
+}
+
+export default Loading;
