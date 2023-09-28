9fcd9b2ca625bdc96f080c0499eebe16f78fb3a4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-28 17:40:58 +0330

    use react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header";
-import WatchOffer from "./components/WatchOffer";
-import NewMoviesList from "./components/NewMoviesList";
-import NewSeriesList from "./components/NewSeriesList";
+
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <WatchOffer /> */}
-      <NewMoviesList />
-      <NewSeriesList />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/movie/:id" element={<Movie />} />
+      </Routes>
+
       <Footer />
     </>
   );
