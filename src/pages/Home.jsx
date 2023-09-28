d11285388ab23751b4f2eab32d671f7b29c31be2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-28 17:40:38 +0330

    create Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import WatchOffer from "../components/WatchOffer";
+import NewMoviesList from "../components/NewMoviesList";
+import NewSeriesList from "../components/NewSeriesList";
+
+function Home() {
+  return (
+    <>
+      {/* <WatchOffer /> */}
+      <NewMoviesList />
+      <NewSeriesList />
+    </>
+  );
+}
+
+export default Home;
