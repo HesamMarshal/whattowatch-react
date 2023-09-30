d394d8319f58a8ede3e1f80d1d871bd368f250f7
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-30 15:20:50 +0330

    use utils for all common address

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,4 +1,14 @@
+// TMDB
+const API_KEY = "api_key=8bde9f388c6e89b90a68fdc2eaddcbf8";
+const BASE_URL = "https://api.themoviedb.org/3";
+
 export const imageURL = "https://image.tmdb.org/t/p/w500/";
+
+// const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&language=fa&" + API_KEY;
+export const movieURL =
+  BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+
+export const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 
 export function getColor(vote) {
   if (vote >= 8) {
--- src/components/NewMoviesList.jsx
+++ src/components/NewMoviesList.jsx
@@ -4,15 +4,16 @@ import { useEffect, useState } from "react";
 import Loading from "./Loading";
 
 import MovieCard from "./MovieCard/MovieCard";
+import { movieURL } from "../Utils/utils";
 
 // TMDB
 
-const API_KEY = "api_key=8bde9f388c6e89b90a68fdc2eaddcbf8";
-const baseURL = "https://api.themoviedb.org/3";
-// const API_URL = baseURL + "/discover/movie?sort_by=popularity.desc&language=fa&" + API_KEY;
-const movieURL = baseURL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+// const API_KEY = "api_key=8bde9f388c6e89b90a68fdc2eaddcbf8";
+// const baseURL = "https://api.themoviedb.org/3";
+// // const API_URL = baseURL + "/discover/movie?sort_by=popularity.desc&language=fa&" + API_KEY;
+// const movieURL = baseURL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 
-const searchURL = baseURL + "/search/movie?" + API_KEY;
+// const searchURL = baseURL + "/search/movie?" + API_KEY;
 
 function NewMoviesList() {
   const [movies, setMovies] = useState([]);
