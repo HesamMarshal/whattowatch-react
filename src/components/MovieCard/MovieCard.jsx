bb3f0c2459c7cd7f3eae5e5843a99e37dd8b00ae
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-30 15:15:25 +0330

    Create Movie Card

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import imagePlaceholder from "../../assets/images/placeholder.jpg";
+import { getColor, imageURL } from "../../Utils/utils";
+
+function MovieCard({ movie }) {
+  const { id: tmdb, title, poster_path, vote_average, overview } = movie;
+
+  return (
+    <Link to={`/movie/${tmdb}`}>
+      <div className="movie">
+        <img
+          src={poster_path ? imageURL + poster_path : imagePlaceholder}
+          alt={title}
+        />
+        <div className="movieInfo">
+          <h3>{title}</h3>
+          <span className={getColor(vote_average)}>{vote_average}</span>
+          <div className="overview">
+            <h3>Overview</h3>
+            {overview}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default MovieCard;
