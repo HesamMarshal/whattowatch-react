3ba8d39da095ad150196a776dc26b205a942a94c
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-30 15:08:04 +0330

    add handler for search to navigate to search page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiMinus, HiPlus, HiSearch } from "react-icons/hi";
+import { Link, createSearchParams, useNavigate } from "react-router-dom";
 
 function Header() {
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = () => {
+    const encodedParams = createSearchParams({
+      search,
+    });
+    navigate({
+      pathname: "/search",
+      search: encodedParams.toString(),
+    });
+  };
   return (
     <div className="header">
       <div className="headerSearch">
         <div className="headerSearchItem">
-          What To Watch
+          <Link to="/">What To Watch</Link>
           <span className="separator"></span>
         </div>
         <div className="headerSearchItem">
-          <BsSearch />
+          {/* <BsSearch /> */}
           <input
             type="text"
             id="searchItem"
@@ -21,6 +34,11 @@ function Header() {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn" onClick={handleSearch}>
+            <HiSearch className="headerIcon" />
+          </button>
         </div>
       </div>
     </div>
