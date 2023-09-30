3b68faf72efb9fd0ddd1dfbac38f9674996dbdec
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-30 15:07:39 +0330

    add search to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-
 import "./App.css";
-import Header from "./components/Header";
-
-import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -16,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movie" element={<Movie />} />
         <Route path="/movie/:id" element={<Movie />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
 
       <Footer />
