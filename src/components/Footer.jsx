2f1dbdfc9cbbbdc237d5bb7814010a032bc3515d
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-09-30 16:12:48 +0330

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,15 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div id="footer">
+      <div className="footerData container">
+        <div>
+          Developed by{" "}
+          <a href="https://hesammarshal.ir">Hesam Marshal Akrami</a>
+        </div>
+        <div>Release 0.01 </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
