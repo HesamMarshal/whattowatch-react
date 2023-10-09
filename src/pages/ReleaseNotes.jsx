25869b9ff98096cc24be1f9116fa5d99777d8314
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-10-09 15:48:44 +0330

    change appearance

--- src/pages/ReleaseNotes.jsx
+++ src/pages/ReleaseNotes.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function ReleaseNotes() {
-  return <div>First Release</div>;
+  return (
+    <section id="release">
+      <div className="release">
+        <h3>Version 0.1.0</h3>
+        <p></p>
+      </div>
+      <div className="release">
+        <h3>Version 0.01</h3>
+        <p>First Release</p>
+      </div>
+    </section>
+  );
 }
 
 export default ReleaseNotes;
