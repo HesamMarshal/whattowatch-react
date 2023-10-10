81cc46caf2ada9c53929688313c2ddf481b35906
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-10-10 13:18:47 +0330

    change home page to show banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import WatchOffer from "../components/WatchOffer";
 import NewMoviesList from "../components/NewMoviesList";
 import NewSeriesList from "../components/NewSeriesList";
+import MovieBanner from "./MovieBanner/MovieBanner";
 
 function Home() {
   return (
     <>
       {/* <WatchOffer /> */}
-      <NewMoviesList />
+      {/* <NewMoviesList /> */}
+      <MovieBanner />
       {/* <NewSeriesList /> */}
     </>
   );
