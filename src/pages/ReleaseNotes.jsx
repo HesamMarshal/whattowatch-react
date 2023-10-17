3eb249c52c5e073f8b5a682a47badbe6d77e6fbb
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-10-17 13:46:41 +0330

    improve visual

--- src/pages/ReleaseNotes.jsx
+++ src/pages/ReleaseNotes.jsx
@@ -1,20 +1,29 @@
+import "./release.css";
 import React from "react";
 
 function ReleaseNotes() {
   return (
-    <section id="release">
-      <div className="release">
-        <h3>Version 0.1.0</h3>
-        <ul>
-          <li>improve styles and appearance </li>
-          <li>Add Directors and Writers</li>
-          <li>Add cast of a movie</li>
-          <li>Add Logo and Favicon</li>
-        </ul>
-      </div>
-      <div className="release">
-        <h3>Version 0.01</h3>
-        <p>First Release</p>
+    <section id="releases">
+      <div className="container">
+        <div className="release">
+          <h3>Version 0.2.0</h3>
+          <ol>
+            <li>Change Color Theme to</li>
+          </ol>
+        </div>
+        <div className="release ">
+          <h3>Version 0.1.0</h3>
+          <ol>
+            <li>improve styles and appearance </li>
+            <li>Add Directors and Writers</li>
+            <li>Add cast of a movie</li>
+            <li>Add Logo and Favicon</li>
+          </ol>
+        </div>
+        <div className="release">
+          <h3>Version 0.01</h3>
+          <p>First Release</p>
+        </div>
       </div>
     </section>
   );
