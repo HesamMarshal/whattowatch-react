bce3be272c770587c8d793d1a850b55cd8be6603
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-10-20 16:44:15 +0330

    add new notes

--- src/pages/ReleaseNotes.jsx
+++ src/pages/ReleaseNotes.jsx
@@ -6,13 +6,16 @@ function ReleaseNotes() {
     <section id="releases">
       <div className="container">
         <div className="release">
-          <h3>Version 0.2.0</h3>
+          <h3>Version 0.2.0:</h3>
           <ol>
-            <li>Change Color Theme to</li>
+            <li>Show the score only with two decimal points</li>
+            <li>Show movie duration by Hour:Minutes</li>
+            <li>Show only the year in title of movie</li>
+            <li>Change color theme to</li>
           </ol>
         </div>
         <div className="release ">
-          <h3>Version 0.1.0</h3>
+          <h3>Version 0.1.0:</h3>
           <ol>
             <li>improve styles and appearance </li>
             <li>Add Directors and Writers</li>
@@ -21,7 +24,7 @@ function ReleaseNotes() {
           </ol>
         </div>
         <div className="release">
-          <h3>Version 0.01</h3>
+          <h3>Version 0.01:</h3>
           <p>First Release</p>
         </div>
       </div>
