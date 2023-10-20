436c4cd72c0614b55cb84ddd1d82878c7a2b1cc4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-10-20 20:45:20 +0330

    add comments

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -4,12 +4,14 @@ const BASE_URL = "https://api.themoviedb.org/3";
 
 export const imageURL = "https://image.tmdb.org/t/p/w500/";
 
-// const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&language=fa&" + API_KEY;
+// movie URLS
 export const movieURL =
   BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 
-export const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+// Series URLS
 
+// Search URLS
+export const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 export const multiSearchURL = BASE_URL + "/search/multi?" + API_KEY;
 
 // export const movieCredits =
